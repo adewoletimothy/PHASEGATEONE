7df5810677a0b6631916ff3f8ae20bc80b7a306b
therabbi.tim@gmail.com <you@example.com>
2025-06-06 21:51:47 +0100

    3 tests done

--- JAVA/MenstrualCalendarTest.java
+++ JAVA/MenstrualCalendarTest.java
@@ -9,11 +9,32 @@ import java.time.format.DateTimeFormatter;
 public class MenstrualCalendarTest{
 
 	@Test
-	public void testStartDate(){  // the test case
+	public void testStartDateFunction(){  // the test case
 	MenstrualCycleFunctions.startDate(LocalDate.of(01,03,25));
-	
 	}
 
+	@Test
+	public void testStartDateFunctionReturnsFormattedDate(){ 
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy");
+	LocalDate actual = MenstrualCycleFunctions.startDate(LocalDate.of(01,03,25));
+	LocalDate expected = LocalDate.parse("01/03/25", formatter);
+	assertEquals(actual, expected);
+	}
+
+	@Test
+	public void testEndDateFunctionReturnsFormattedDate(){ 
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy");
+	LocalDate actual = MenstrualCycleFunctions.endDate(LocalDate.of(29,03,25));
+	LocalDate expected = LocalDate.parse("29/03/25" , formatter);
+	assertEquals(actual, expected);
+	}
+
+	@Test
+	public void testReturnDaysBetweenStartAndEndDate(){ 
+	int actual = MenstrualCycleFunctions.menstrualCycle(01,03,25 , 29,03,25);
+	int expected = 28; 
+	assertEquals(actual, expected);
+	}
 
 
 }
--- JAVA/MenstrualCycleFunctions.java
+++ JAVA/MenstrualCycleFunctions.java
@@ -1,26 +1,44 @@
-//java
-
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 
 public class MenstrualCycleFunctions{
 
 	public static LocalDate startDate(LocalDate date){
 
-	String startDate = "1/03/25";
+	String startDate = "01/03/25";
 	
-	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyy");
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy");
 
-	LocalDate herDate = LocalDate.parse(startDate, formatter);
+	LocalDate herStartDate = LocalDate.parse(startDate, formatter);
 
-	return herDate;
+	return herStartDate;
 	}
 
 
+	public static LocalDate endDate(LocalDate date){
+
+	String endDate = "29/03/25";
+	
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy");
+
+	LocalDate herEndDate = LocalDate.parse(endDate, formatter);
+
+	return herEndDate;
+	}
 
 
+	public static int menstrualCycle(LocalDate startDate, LocalDate endDate){
 
+	startDate = LocalDate.of(01,03,25);
+        
+	endDate = LocalDate.of(29,03,25);
+
+        int daysInBetween = (int)ChronoUnit.DAYS.between(startDate, endDate);
+
+	return daysInBetween;
+	}
 
 
 
